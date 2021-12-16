19b16b1cf4ca8b732e839344e05064cb78af364c
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-16 23:46:30 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -916,7 +916,7 @@ module.exports = handle = (client, Client) => {
  	                 const mediaMsg = await client.prepareMessageMedia(await getBuffer(configs.imgUrl), 'imageMessage')
                      const buttonMessage = {
                            contentText: menu(data.prefix, data.pushname),
-                           footerText: 'TIO-OFFICIAL彡',
+                           footerText: 'HOSTING-TioBotz',
                                 "contextInfo": {
 									  mentionedJid: [configs.ownerList[0]],
                                       participant: sender,
